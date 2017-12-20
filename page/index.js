cc1b30cafe9c4afb9ac68a2e1fd8a1bb8b4deae2
wangweihua <aztack@163.com>
2017-12-20 14:45:25 +0800

    add page generator

---
+++ page/index.js
@@ -0,0 +1,40 @@
+const Generator = require('yeoman-generator')
+require('colors')
+
+module.exports = class extends Generator {
+  constructor(args, opts) {
+    super(args, opts);
+  }
+
+  initializing() {
+    this.pkg = require('../package.json');
+  }
+
+  prompting() {
+
+    const prompts = [{
+      type: 'input',
+      name: 'name',
+      message: 'Please input page name:',
+    }];
+
+    return this.prompt(prompts).then(answers => {
+      this.name = answers.name;
+    });
+  }
+
+  writing() {
+    var capitalizedName = this.name[0].toUpperCase() + this.name.slice(1)
+    this.fs.copyTpl(
+      this.templatePath('index.vue'),
+      this.destinationPath(`src/js/pages/${this.name}/${capitalizedName}.vue`), {
+        name: this.name
+      }
+    )
+    this.log(`1) import create page in _routes.js:`.red.bold)
+    this.log(`import ${capitalizedName} from './pages/${this.name}/${capitalizedName}.vue'`)
+    this.log('')
+    this.log(`2) point 'path' to created page component:`.red.bold)
+    this.log(`{ path: '/paht/to/page', component: ${capitalizedName}, props: {} }`)
+  }
+};
