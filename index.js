a5456a0f6bf0052cf086b89af9e0ec6c454db6dc
wangweihua <aztack@163.com>
2017-12-20 17:04:58 +0800

    add packages

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ module.exports = class extends Generator {
   }
 
   initializing() {
-    // this.pkg = require('../package.json');
+    this.pkg = require('../package.json')
+    this.log('mpa')
   }
 };
