c8291f3f721f3bc938a232339108828566facdf9
wangweihua <aztack@163.com>
2017-12-22 18:25:18 +0800

    add dialog generator

---
+++ dialog/index.js
@@ -0,0 +1,144 @@
+const Generator = require('yeoman-generator')
+const glob = require('glob')
+const path = require('path')
+require('colors')
+
+
+module.exports = class extends Generator {
+  constructor(args, opts) {
+    super(args, opts);
+  }
+
+  initializing() {
+    this.pkg = require('../package.json')
+    this._readPageDirs()
+  }
+
+  prompting() {
+
+    const prompts = [{
+      type: 'list',
+      name: 'type',
+      message: 'Please select',
+      choices: [{
+        name: 'A common dialog component (src/components):',
+        value: 'common',
+        short: 'common'
+      }, {
+        name: 'A dialog for page (src/pages/???/):',
+        value: 'page',
+        short: 'page'
+      }]
+    }, {
+      type: 'list',
+      name: 'pageName',
+      message: 'which page?',
+      when: answers => answers.type === 'page',
+      choices : this.pages
+    }, {
+      type: 'input',
+      name: 'dialogName',
+      message: 'dialog name, dash-separated, not "-dialog" suffix:'
+    }, {
+      type: 'list',
+      name: 'dialogSize',
+      message: 'dialog size?',
+      default: 'small',
+      choices: [{
+        name: 'tiny',
+        value: 'tiny'
+      }, {
+        name: 'small',
+        value: 'small',
+      }, {
+        name: 'large',
+        value: 'large'
+      }, {
+        name: 'full',
+        value: 'full'
+      }]
+    }]
+
+    return this.prompt(prompts).then(answers => {
+      this.type = answers.type
+      this.page = answers.pageName
+      this.name = answers.dialogName + '-dialog'
+      this.size = answers.dialogSize
+    })
+  }
+
+  writing() {
+    const locals = {name: this.name, size: this.size}
+    const camelizedName = camelize(this.name)
+    var dest = `src/components/${this.name}.vue`
+    if (this.type === 'page') {
+      dest = `src/js/pages/${this.page}/${this.name}.vue`
+    }
+    this.fs.copyTpl(
+      this.templatePath('dialog.vue'),
+      this.destinationPath(dest),
+      locals
+    )
+
+    this.log(`select part of the following code and insert it to where needed`.red.bold)
+    this.log(`
+      <template>
+        <!-- html -->
+        <${this.name}
+          title="Dialog Title",
+          :visible="isOpen${camelizedName}
+          size="${this.size}",
+          @action="on${camelizedName}Action">
+        </${this.name}>
+
+        <!-- jade -->
+        ${this.name}(
+          title="Dialog Title",
+          :visible="isOpen${camelizedName}",
+          size="${this.size}",
+          @action="on${camelizedName}Action")
+      </template>
+
+      <script>
+      import ${camelizedName} from './${this.name}.vue'
+      export default {
+        components: {
+          Component,
+          ${camelizedName}
+        },
+        data () {
+          data,
+          isOpen${camelizedName}: true
+        },
+        methods: {
+          method () {
+
+          },
+          on${camelizedName}Action (act) {
+            console.log(act)
+            this.isOpen${camelizedName} = false
+          }
+        }
+      }
+      </script>
+    `.green.bold)
+  }
+  _readPageDirs () {
+    const root = this.destinationRoot()
+    this.pages = glob.sync(`${root}/src/js/pages/*/`).reduce(function (all, page) {
+      const name = path.basename(page)
+      all.push({
+        name: name,
+        value: name,
+        short: name
+      })
+      return all
+    }, [])
+  }
+}
+
+function camelize(s) {
+  return s.replace(/(^[a-z]|[-_]([a-z]))/g, function(_, a, b) {
+    return (b || a).toUpperCase()
+  })
+}
---
+++ dialog/templates/dialog.vue
@@ -0,0 +1,50 @@
+<template lang="jade">
+  .<%=name%>
+    el-dialog(v-bind="$vnode.data.attrs")
+      .content TODO: Dialog content here
+      span.dialog-footer(slot="footer")
+        el-button(@click="onButtonClicked('cancel')") Cancel
+        el-button(type="primary", @click="onButtonClicked('ok')") OK
+</template>
+
+<style lang="stylus" scoped>
+
+</style>
+
+<script>
+// framework imports
+import ElDialog from 'element-ui/lib/dialog'
+import ElButton from 'element-ui/lib/button'
+// components imports
+
+// utils imports
+
+export default {
+  name: '<%=name%>',
+  components: {
+    ElDialog,
+    ElButton
+  },
+  props: [
+    // 'data'
+  ],
+  data () {
+    return {
+
+    }
+  },
+  methods: {
+    // initializer
+    init () {
+
+    },
+    // event handlers
+    onButtonClicked (whichButton) {
+      this.$emit('action', whichButton)
+    }
+  },
+  watch: {
+
+  }
+}
+</script>
