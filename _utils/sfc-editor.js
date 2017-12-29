02aacc34486a1c93be6af61962390472023c9baf
wangweihua <aztack@163.com>
2017-12-29 17:04:15 +0800

    use babel builder to insert code

--- _utils/sfc-editor.js
+++ _utils/sfc-editor.js
@@ -1,11 +1,10 @@
 // SFC parser
 const parse = require('vue-loader/lib/parser')
 // babel
-const babylon = require('babylon')
+const babel = require('babel-core')
 const t = require('babel-types')
-const traverse = require('babel-traverse').default
+const template = require('babel-template')
 // misc
-const prettier = require('prettier')
 const fs = require('fs')
 
 /**
@@ -14,7 +13,7 @@ const fs = require('fs')
  */
 function SfcEditor (srcPath) {
   this.srcPath = srcPath
-  const src = fs.readFileSync(srcPath).toString()
+  const src = this.source = fs.readFileSync(srcPath).toString()
   this.parts = parse(src)
 }
 
@@ -25,23 +24,23 @@ function SfcEditor (srcPath) {
  */
 SfcEditor.prototype.appendImport = function (importCode) {
   const js = this.parts.script.content
-  const ast = babylon.parse(js, {
-    sourceType: 'module'
-  })
-  var n
-  traverse(ast, {
-    ImportDeclaration: {
-      exit(path) {
-        n = path.node
+  this.parts.script.content = babel.transform(js, {
+    plugins: [{
+      visitor: {
+        Program(path, state) {
+          const body = path.get('body')
+          const importDecl = template(importCode, {sourceType: 'module'})()
+
+          if (!tryInsert(body, 'isImportDeclaration', 'After', importDecl)) {
+            if (!tryInsert(body, 'isStatement', 'Before', importDecl)) {
+              throw new Error('Can not found export default declaration!')
+            }
+          }
+        }
       }
-    }
-  })
-  if (n) {
-    const inserLine = n.loc.start.line
-    const code = js.split("\n")
-    code.splice(inserLine, 0, importCode)
-    parts.script.content = prettier.format(code.join("\n"))
-  }
+    }]
+  }).code
+  this._update('script')
   return this
 }
 
@@ -53,38 +52,52 @@ SfcEditor.prototype.appendImport = function (importCode) {
  */
 SfcEditor.prototype.appendMethod = function (name, args) {
   const js = this.parts.script.content
-  const ast = babylon.parse(js, {
-    sourceType: 'module'
-  })
-  var n
-  traverse(ast, {
-    ExportDefaultDeclaration: {
-      exit(path1) {
-        path1.traverse({
-          ObjectProperty: {
-            exit (path2) {
-              if (path2.node.key.name === 'methods') {
-                const props = path2.node.value.properties
-                n = props[props.length - 1]
-              }
+  babel.transform(js, {
+    plugins: [{
+      visitor: {
+        ExportDefaultDeclaration(path, state) {
+          if(t.isObjectExpression(path.node.declaration)) {
+            const props = path.node.declaration.properties
+            const methodsPath = props.filter(p => t.isObjectProperty(p) && p.key.name === 'methods').shift()
+            if (methodsPath) {
+              const m = t.objectMethod('method', t.Identifier(name), [], t.BlockStatement([]))
+              methodsPath.value.properties.push(m)
             }
           }
-        })
+        }
       }
-    }
+    }]
   })
-  if (n) {
-    const inserLine = n.loc.end.line
-    const code = js.split("\n")
-    code.splice(inserLine, 0, `,${name} (${args}) {\n// TODO:\n}`)
-    parts.script.content = prettier.format(code.join("\n"))
-  }
+  this._update('script')
   return this
 }
 
-SfcEditor.prototype.generate = function () {
+/**
+ * update this.parts from transformed content
+ * @param  {String} which ['styles' | 'template' | 'script']
+ * @return {SfcEditor}
+ */
+SfcEditor.prototype._update= function (which) {
+  var src = this.source
+  const pos = this.parts[which]
+  var before = src.substring(0, pos.start)
+  var after = src.substring(pos.end)
 
+  if (which === 'script') {
+    let s = this.parts.script
+    s.content = s.content
+      .split('\n')
+      .filter(line => !line.match(/^\/\/$/)).join('\n').trim()
+  }
+
+  before = appendNewline(before)
+  after = prependNewline(after)
+
+  src = [before, this.parts[which].content, after].join('')
+  this.parts = parse(this.source = src)
+  return this
 }
+
 
 /**
  * save modified code back to vue file
@@ -92,6 +105,23 @@ SfcEditor.prototype.generate = function () {
 SfcEditor.prototype.save = function () {
   const newCode = this.generate()
   return fs.writeFileSync(this.srcPath, newCode)
+}
+
+function tryInsert(body, insertPointType, where, ast) {
+  const insertPoint = body.filter(p => p[insertPointType]()).pop()
+  if (insertPoint) {
+    insertPoint['insert' + where](ast)
+    return true
+  }
+  return false
+}
+
+function appendNewline (s) {
+  if (s[s.length - 1] != '\n') return s + '\n'
+}
+
+function prependNewline (s) {
+  if (s[0] != '\n') return '\n' + s
 }
 
 module.exports = SfcEditor
