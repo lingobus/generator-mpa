ec17291adc74db2855c261aae1dedb8e961451a2
wangweihua <aztack@163.com>
2018-02-17 12:22:45 +0800

    feat(sfc-editor):addDateFields

--- _utils/sfc-editor.js
+++ _utils/sfc-editor.js
@@ -33,7 +33,7 @@ SfcEditor.prototype.appendImport = function (importCode) {
 
           if (!tryInsert(body, 'isImportDeclaration', 'After', importDecl)) {
             if (!tryInsert(body, 'isStatement', 'Before', importDecl)) {
-              throw new Error('Can not found export default declaration!')
+              throw new Error('sfc-editor:Can not found export default declaration!')
             }
           }
         }
@@ -59,12 +59,15 @@ SfcEditor.prototype.appendMethod = function (name, args) {
           if(t.isObjectExpression(path.node.declaration)) {
             const props = path.node.declaration.properties
             const methodsNode = props.filter(p => t.isObjectProperty(p) && p.key.name === 'methods').shift()
-            const m = t.objectMethod('method', t.Identifier(name), [], t.BlockStatement([]))
+            const argus = args.split(',').map(arg => t.Identifier(arg))
+            const m = t.objectMethod('method', t.Identifier(name), argus, t.BlockStatement([]))
             if (methodsNode) {
               methodsNode.value.properties.push(m)
             } else {
               props.push(t.objectProperty(t.Identifier('methods'), t.objectExpression([m])))
             }
+          } else {
+            throw new Error('sfc-editor:export default declaration is not a object expression')
           }
         }
       }
@@ -73,6 +76,71 @@ SfcEditor.prototype.appendMethod = function (name, args) {
   this._update('script')
   return this
 }
+
+SfcEditor.prototype.appendComponent = function (tag, comp) {
+  if (arguments.length === 1) comp = tag
+  const js = this.parts.script.content
+  this.parts.script.content = babel.transform(js, {
+    plugins: [{
+      visitor: {
+        ExportDefaultDeclaration(path) {
+          if(t.isObjectExpression(path.node.declaration)) {
+            const props = path.node.declaration.properties
+            const node = props.filter(p => t.isObjectProperty(p) && p.key.name === 'components').shift()
+            const c = t.ObjectProperty(t.StringLiteral(tag), t.Identifier(comp))
+            if (node) {
+              node.value.properties.push(c)
+            } else {
+              props.push(t.ObjectProperty(t.Identifier('components'), t.objectExpression([c])))
+            }
+          } else {
+            throw new Error('sfc-editor:export default declaration is not a object expression')
+          }
+        }
+      }
+    }]
+  }).code
+  this._update('script')
+  return this
+}
+
+
+SfcEditor.prototype.addDataFields = function (fields) {
+  const js = this.parts.script.content
+  this.parts.script.content = babel.transform(js, {
+    plugins: [{
+      visitor: {
+        ExportDefaultDeclaration(path) {
+          if(t.isObjectExpression(path.node.declaration)) {
+            const props = path.node.declaration.properties
+            var node = props.filter(p => t.isObjectMethod(p) && p.key.name === 'data').shift()
+            if (!node) {
+              node = props.filter(p => t.isObjectProperty(p) && p.key.name === 'data').shift()
+              if (node) {
+                throw new Error('sfd-editor:data should be a function, got an object ')
+              }
+            }
+            const retSta = node.body.body.filter(n => t.isReturnStatement(n)).shift()
+            if (!retSta || retSta.argument === null || !t.isObjectExpression(retSta.argument)) {
+              throw new Error('sfc-editor:data function did not return an object!')
+            }
+            const retObjProps = retSta.argument.properties
+            for(let k in fields) {
+              let v = fields[k]
+              const val = getNodeFromValue(v)
+              retObjProps.push(t.ObjectProperty(t.Identifier(k), val))
+            }
+          } else {
+            throw new Error('sfc-editor:export default declaration is not a object expression')
+          }
+        }
+      }
+    }]
+  }).code
+  this._update('script')
+  return this
+}
+
 
 /**
  * update this.parts from transformed content
@@ -124,6 +192,39 @@ function appendNewline (s) {
 
 function prependNewline (s) {
   if (s[0] != '\n') return '\n' + s
+}
+
+/**
+ * create babel node from given value
+ * @param  {any} val
+ * @return {BabelNode}
+ * @remark
+ * RegExpLiteral
+ *   | NullLiteral
+ *   | StringLiteral
+ *   | BooleanLiteral
+ *   | NumericLiteral
+ */
+function getNodeFromValue (val) {
+  const type = typeof val
+  if (val === null || type === 'undefined') {
+    return t.NullLiteral()
+  } else if (type === 'number' || val instanceof Number) {
+    return t.NumericLiteral(val)
+  } else if (type === 'boolean' || val instanceof Boolean) {
+    return t.BooleanLiteral(val)
+  } else if (type === 'string' || val instanceof String) {
+    return t.StringLiteral(val)
+  } else if (val instanceof RegExp) {
+    return t.RegExpLiteral(val.source, val.flags)
+  } else if (type === 'function') {
+    // anonymouse function expression can not be parsed
+    // make a var declaration from it
+    const code = 'var a = ' + val.toString()
+    const ast = template(code, {sourceType: 'module'})()
+    return ast.declarations[0].init
+  }
+  return t.NullLiteral()
 }
 
 module.exports = SfcEditor
--- _utils/sfc-editor.test.js
+++ _utils/sfc-editor.test.js
@@ -1,6 +1,14 @@
 const $path = require('path')
 const SfcEditor = require('./sfc-editor.js')
 const editor = new SfcEditor($path.resolve(__dirname, '../page/templates/index.vue'))
-// editor.appendImport('//\nimport Dialog from "./dialog.vue"')
+editor.appendImport('//\nimport Dialog from "./dialog.vue"')
 editor.appendMethod('hehe', 'a,b,c')
+editor.appendComponent('CreateTeacherDialog')
+editor.addDataFields({
+  isVisible: false,
+  Pi: 3.14,
+  pattern: /https?/gi,
+  name: 'string',
+  f: function (a,b,c) {return a+b+c+123}
+})
 console.log(editor.source)
